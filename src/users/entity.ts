26079a1073a236b47df13d6eeff40d8d140f4981
lisamariedekker <lisamariedekker@outlook.com>
2018-11-20 15:55:32 +0100

    added validation

--- src/users/entity.ts
+++ src/users/entity.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 import { BaseEntity } from 'typeorm/repository/BaseEntity'
+import { IsString, MinLength, IsEmail } from 'class-validator';
 
 @Entity()
 export default class User extends BaseEntity {
@@ -7,15 +8,22 @@ export default class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id?: number
 
+  @IsString()
+  @MinLength(2)
   @Column('text', {nullable:false})
   firstName: string
 
+  @IsString()
+  @MinLength(2)
   @Column('text', {nullable:false})
   lastName: string
 
+  @IsEmail()
   @Column('text', {nullable:false})
   email: string
 
+  @IsString()
+  @MinLength(3)
   @Column('text')
   city: string
 }
